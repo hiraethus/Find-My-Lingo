51627e09daa1c1abbbefea68ec1a50030d6651a6
Michael Jones <mjclackjones@gmail.com>
2016-07-24 15:31:16 +0100

    Move GwasanaethController to com.clackjones.cymraeg.GwasanaethController package

--- src/main/java/com/clackjones/cymraeg/gwasanaeth/web/GwasanaethController.java
+++ src/main/java/com/clackjones/cymraeg/gwasanaeth/web/GwasanaethController.java
@@ -1,8 +1,7 @@
-package com.clackjones.cymraeg;
+package com.clackjones.cymraeg.gwasanaeth.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,6 +15,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.validation.Valid;
 import java.util.*;
 import java.util.stream.Collectors;
+
+import com.clackjones.cymraeg.*;
 
 @Controller
 @RequestMapping("/gwasanaethau/*")
