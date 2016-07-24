5e380f61c5998c0c7434a18b0204e96c1f190469
Michael Jones <mjclackjones@gmail.com>
2016-07-24 15:37:14 +0100

    Move LoginController to com.clackjones.cymraeg.login.web package

--- src/main/java/com/clackjones/cymraeg/login/web/LoginController.java
+++ src/main/java/com/clackjones/cymraeg/login/web/LoginController.java
@@ -1,4 +1,4 @@
-package com.clackjones.cymraeg;
+package com.clackjones.cymraeg.login.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
