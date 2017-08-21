60876e91b9e40099f60e369531a83ea68eda01e6
Mike Clack Jones <mjclackjones@gmail.com>
2017-08-21 03:29:17 +0100

    Remove stray import in GetTheDataLocationService

--- src/main/java/com/clackjones/cymraeg/address/GetTheDataLocationService.java
+++ src/main/java/com/clackjones/cymraeg/address/GetTheDataLocationService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.Optional;
 
