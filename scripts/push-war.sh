a8f6bf2965557f911efb105afd59424829864282
Mike Clack Jones <mjclackjones@gmail.com>
2018-11-13 00:45:35 +0000

    Amend push-war.sh

--- scripts/push-war.sh
+++ scripts/push-war.sh
@@ -2,3 +2,5 @@
 
 # Run to deploy ROOT.war to Vagrant
 vagrant ssh -c 'sudo cp  /findmylingo/target/ROOT.war /var/lib/tomcat/webapps/'
+vagrant ssh -c 'sudo systemctl stop tomcat'
+vagrant ssh -c 'sudo systemctl start tomcat'
