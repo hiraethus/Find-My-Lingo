24b7f2d3c61f2628dea325403fbab2f12091d413
Mike Clack Jones <mjclackjones@gmail.com>
2019-01-24 05:13:19 +0000

    Started creating findmylingo-common submodule

--- findmylingo-webapp/src/main/java/com/clackjones/cymraeg/gwasanaeth/web/GwasanaethController.java
+++ findmylingo-webapp/src/main/java/com/clackjones/cymraeg/gwasanaeth/web/GwasanaethController.java
@@ -6,12 +6,10 @@ import com.clackjones.cymraeg.language.LanguageService;
 import com.clackjones.cymraeg.opengraph.OpenGraphDataGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
@@ -23,7 +21,6 @@ import javax.naming.NoPermissionException;
 import javax.validation.Valid;
 import java.security.Principal;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/")
