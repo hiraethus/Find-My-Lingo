17c8b92710c934bcf5b081aebad4fc9a582150e7
Mike Clack Jones <mjclackjones@gmail.com>
2019-02-07 22:01:25 +0000

    Make fat jar for findmylingo-admintools

--- findmylingo-admintools/src/main/kotlin/com/clackjones/cymraeg/admin/Main.kt
+++ findmylingo-admintools/src/main/kotlin/com/clackjones/cymraeg/admin/Main.kt
@@ -3,19 +3,13 @@ package com.clackjones.cymraeg.admin
 import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.core.env.SimpleCommandLinePropertySource
 
-class Main {
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            val cliPropertySource = SimpleCommandLinePropertySource()
+fun main(args: Array<String>) {
+    val cliPropertySource = SimpleCommandLinePropertySource()
 
-            val appCtxt  = AnnotationConfigApplicationContext()
-            appCtxt.environment.propertySources.addFirst(cliPropertySource)
-            appCtxt.register(AppConfig::class.java)
-            appCtxt.refresh()
+    val appCtxt  = AnnotationConfigApplicationContext()
+    appCtxt.environment.propertySources.addFirst(cliPropertySource)
+    appCtxt.register(AppConfig::class.java)
+    appCtxt.refresh()
 
-            appCtxt.getBean(OtherClass::class.java).printString()
-        }
-    }
+    appCtxt.getBean(OtherClass::class.java).printString()
 }
-
