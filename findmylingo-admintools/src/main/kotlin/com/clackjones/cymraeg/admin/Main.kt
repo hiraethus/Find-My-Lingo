31c28cd35512e309ceaca27377a106265dc9cfe9
Mike Clack Jones <mjclackjones@gmail.com>
2019-02-13 23:27:08 +0000

    findmylingo-admintools successfuly create admin user from commandline

--- findmylingo-admintools/src/main/kotlin/com/clackjones/cymraeg/admin/Main.kt
+++ findmylingo-admintools/src/main/kotlin/com/clackjones/cymraeg/admin/Main.kt
@@ -1,26 +1,39 @@
 package com.clackjones.cymraeg.admin
 
+import com.clackjones.cymraeg.user.RegistrationDetails
+import com.clackjones.cymraeg.user.RegistrationService
 import org.springframework.context.support.beans
 import org.springframework.context.support.GenericApplicationContext
 import org.springframework.beans.factory.getBean
+import org.springframework.core.io.ClassPathResource
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader
+import java.util.Locale
 
-class GreetingProvider(_greeting: String) {
-    val greeting = _greeting
-}
-
-class GreetingPrinter(private val _greetingProvider: GreetingProvider) {
-    fun printGreeting() = println(_greetingProvider.greeting)
-}
-
-fun main(args: Array<String>) {
+fun main() {
     val myBeans = beans {
-        bean { GreetingProvider("Hello, there!") }
-        bean { GreetingPrinter( ref<GreetingProvider>())}
+        bean("dbDialect"){"org.hibernate.dialect.H2Dialect"}
+        bean("imgRootDirectory") { java.nio.file.Paths.get("/var/www/findmylingo.local/static/service/images")}
     }
 
     val context = GenericApplicationContext()
+
+    // load the application context for findmylingo-common first
+    val xmlReader = XmlBeanDefinitionReader(context)
+    xmlReader.loadBeanDefinitions(ClassPathResource("META-INF/findmylingo-common-context.xml"))
+
     myBeans.initialize(context)
     context.refresh()
 
-    context.getBean<GreetingPrinter>().printGreeting()
+    val registrationService = context.getBean<RegistrationService>()
+    registerAdmin(registrationService)
+}
+
+fun registerAdmin(registrationService: RegistrationService) {
+    val regDetails = RegistrationDetails()
+    regDetails.nickname = "Admin User2"
+    regDetails.password = "MyVerySecretPassword"
+    regDetails.passwordSecondTimeEntered = "MyVerySecretPassword"
+    regDetails.username = "admin2@example.com"
+
+    registrationService.registerAdmin(regDetails, Locale.ENGLISH)
 }
