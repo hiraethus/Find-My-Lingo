4ac5c655d384a3498a25aa5763723284143bab84
Mike Clack Jones <mjclackjones@gmail.com>
2019-05-06 14:24:41 +0100

    Adding/Removing Categories controlled by authority ADD_REMOVE_CATEGORY

--- findmylingo-webapp/src/main/kotlin/com/clackjones/cymraeg/gwasanaeth/CategoryController.kt
+++ findmylingo-webapp/src/main/kotlin/com/clackjones/cymraeg/gwasanaeth/CategoryController.kt
@@ -1,5 +1,6 @@
 package com.clackjones.cymraeg.gwasanaeth
 
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -9,10 +10,12 @@ class CategoryController(val categoriManager: CategoriManager) {
     fun getAllCategories(): List<Categori> =
             categoriManager.findAll()
 
+    @PreAuthorize("hasAuthority('ADD_REMOVE_CATEGORY')")
     @PostMapping("/{categoryName}")
     fun addNewCategory(@PathVariable("categoryName") categoryName: String) =
             categoriManager.saveCategori(categoryName)
 
+    @PreAuthorize("hasAuthority('ADD_REMOVE_CATEGORY')")
     @DeleteMapping("/{categoryName}")
     fun removeCategory(@PathVariable("categoryName") categoryName: String) =
             categoriManager.removeCategori(categoryName)
